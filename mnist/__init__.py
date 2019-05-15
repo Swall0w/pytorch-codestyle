469d48122d19b64ffbdbfb41b2ce1abcf724bdba
Swall0w <technext.jpn@gmail.com>
2019-05-15 19:32:31 +0900

    [add]: initial files.

---
+++ mnist/__init__.py
@@ -0,0 +1,10 @@
+__copyright__ = 'Copyright (C) 2019 Swall0w'
+__version__ = '0.0.0'
+__author__ = 'Swall0w'
+__url__ = 'https://github.com/Swall0w/pytorch-codestyle'
+
+from mnist import model
+from mnist import utils
+
+__all__ == ['model', 'utils']
+
